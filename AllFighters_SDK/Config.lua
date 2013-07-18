f14d406a737b65638f42cc108e8cba99762ea1fe
Hyro Vitaly Protago <musty.magic@gmail.com>
2013-07-19 00:26:13 +0200

    GithubLoader

---
+++ AllFighters_SDK/Config.lua
@@ -0,0 +1,14 @@
+-- AllFighters_SDK config
+SPRITE_DIRECTORY = "SpriteAllFighters/"
+SPRITE_DIRECTORY_FULL = "Dropbox:" .. SPRITE_DIRECTORY
+SDK_DIRECTORY = SPRITE_DIRECTORY_FULL .. "SDK/"
+FILE_LOCATION = os.getenv("HOME").."/Documents/Dropbox.spritepack/"
+FIGHTERS_XML = FILE_LOCATION .. SPRITE_DIRECTORY .. "/fighters.xml"
+--
+
+-- AllFighters config
+SHOW_SNAP = true
+SHOW_BOXS = true
+SHOW_BOXS_FIGHTER = true
+DEV_SPRITE = false
+--
