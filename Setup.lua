efd04b9853aaf98a27f48073be8f0d0432c68ab4
HyroVitalyProtago <musty.magic@gmail.com>
2013-07-22 23:26:30 +0200

    Update Setup.lua

---
+++ Setup.lua
@@ -0,0 +1,33 @@
+-- AllFighters
+
+-- Dependencies
+    -- BaseGameEngine
+    -- Tools
+
+PROJECTNAME = "AllFighters"
+DESCRIPTION = "Plateform/Fighting Game inspired by 'Jump Ultimate Star' and 'Super Smash Bross'."
+VERSION = "0.0.3"
+
+--GITHUB = "https://raw.github.com/"
+--ALL_FIGHTERS = "HyroVitalyProtago/AllFighters/master/"
+SPRITES = "SpriteAllFighters/"
+TAKESHI_YAMAMOTO = "TakeshiYamamoto/"
+
+SPRITES_LOCATION = "Dropbox:" .. SPRITES
+
+function setup()
+
+    saveProjectInfo("Author", "Hyro Vitaly Protago")
+    saveProjectInfo("Description", DESCRIPTION)
+    displayMode(FULLSCREEN_NO_BUTTONS)
+    
+    parameter.boolean("SHOW_SNAP", false)
+    parameter.boolean("SHOW_BOXS", true)
+    parameter.boolean("SHOW_BOXS_FIGHTER", false)
+    parameter.boolean("DEV_SPRITE", false)
+    parameter.number("scal", 0, 5, 1)
+    FPS = 0
+    
+    game = Game()
+    mkeyboard = Keyboard()
+    MakeTakeshiYamamoto.run()
