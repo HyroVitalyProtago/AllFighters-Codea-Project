4536d083e0d5179c18112186a747ddb311ab004a
Hyro Vitaly Protago <musty.magic@gmail.com>
2013-07-27 19:16:07 +0200

    sugars inheritance

--- Engine/GUI/Templates/MadeWithCodea.lua
+++ Engine/GUI/Templates/MadeWithCodea.lua
@@ -1,4 +1,5 @@
 MadeWithCodea = class(Screen)
+MadeWithCodea.sugars = Screen.sugars
 
 function MadeWithCodea:init(screenTo)
     Screen.init(self, "MadeWithCodea")
@@ -12,9 +13,39 @@ function MadeWithCodea:start()
     end)
 end
 
-function MadeWithCodea:__index(k)
-	Screen.__index(self, k)
+CLASS = MadeWithCodea
+function CLASS:__index(k)
+    if CLASS[k] then return CLASS[k] end
+    for sugarKey, key in pairs(CLASS.sugars) do
+        if k == sugarKey then
+            local arr = table.explode(".", key)
+            if #arr == 1 then
+                return self[key]
+            else
+                local result = self
+                for _,v in pairs(arr) do
+                    result = result[v]
+                end
+                return result
+            end
+        end
+    end
 end
-function MadeWithCodea:__newindex(k, v)
-	Screen.__newindex(self, k, v)
+function CLASS:__newindex(k, v)
+    for sugarKey, key in pairs(CLASS.sugars) do
+        if k == sugarKey then
+            local arr = table.explode(".", key)
+            if #arr == 1 then
+                rawset(self, key, v)
+            else
+                local result = self
+                for _,v in pairs(arr) do
+                    beforeLastResult = result
+                    result = result[v]
+                end
+                rawset(beforeLastResult, arr[#arr], v)
+            end
+        end
+    end
+    rawset(self, k, v)
 end
