2f127bbd0e2f25205264cd22c1e221e208777efb
Hyro Vitaly Protago <musty.magic@gmail.com>
2013-08-04 00:16:20 +0200

    tools test

--- Tools/Image.lua
+++ Tools/Image.lua
@@ -5,7 +5,9 @@ function image(width, height, fdraw)
     local img = ___image(width, height)
     if fdraw then
         setContext(img)
+        pushMatrix()
         fdraw(width, height)
+        popMatrix()
         setContext()
     end
     return img
