7da4047b04ba1da1e31b0e12a584bf932b182a04
Hyro Vitaly Protago <musty.magic@gmail.com>
2013-08-03 20:25:58 +0200

    iselect test

--- Engine/GUI/Inputs/ISelect.lua
+++ Engine/GUI/Inputs/ISelect.lua
@@ -59,6 +59,7 @@ function ISelect:init(args)
         local w2 =  w/7
         local w3 = (w2 - 2*(w2/5))/2 + (w2/5)
         line(w - w2, 0, w - w2, h)
+        strokeWidth(self.strokeWidth)
         line(w - w2 + w2/5, h - h/5, w - w3, h/5)
         line(w - w2/5, h - h/5, w - w3, h/5)
     end)
